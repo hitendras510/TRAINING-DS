992761b11dce6dddfa8a1dbe73449cf4e32b3cad
Hitendra singh <hitendrasingh5489@gmail.com>
2025-09-03 12:53:16 +0530

    new

---
+++ L5/L6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int num = 146;
+    int res = 0;
+
+    while (num != 0)
+    {
+        int lastDigit = num % 10;
+        res = res * 10 + lastDigit;
+        num = num / 10;
+    }
+
+    printf("The reversed  number is: %d\n", res);
+
+    return 0;
+}
