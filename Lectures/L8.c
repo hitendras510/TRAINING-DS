0957a858b301b97a88013d2d9dfe03861d5377af
Hitendra singh <hitendrasingh5489@gmail.com>
2025-09-09 13:22:06 +0530

    new

--- Lectures/L8.c
+++ Lectures/L8.c
@@ -12,17 +12,55 @@
 //     }
 // }
 
+// #include <stdio.h>
+// int main()
+// {
+//     int num=65;
+//     for (int i = 0; i < 4; i++)
+//     {
+//         for (int j = 3; j>i; j--)
+//         {
+//             printf(" ");
+//         }
+//         {
+//             for (int k = 0; k<=i;k++)
+//             {
+//                 printf("*");
+//             }
+//         }
+//         {
+//             for (int p = 1; p <= i; p++)
+//             {
+//                 printf("*");
+//             }
+//         }
+//         printf("\n");
+//     }
+// }
+
 #include <stdio.h>
 int main()
 {
-    int num=65;
-    for (int i = 0; i < 3; i++)
+    int num = 65;
+    for (int i = 0; i < 4; i++)
     {
-        for (int j = 0; j <=i; j++)
+        for (int j = 1; j <= i; j++)
         {
-            printf("%c",num++);
+            printf("*");
         }
-        printf("\n");
     }
+    {
+        for (int k = 0; k <= i; k++)
+        {
+            printf("*");
+        }
+    }
+    {
+        for (int p = 3; p > i; p--)
+        {
+            printf("*");
+        }
+    }
+    printf("\n");
 }
-
+}
