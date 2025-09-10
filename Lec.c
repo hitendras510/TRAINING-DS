88e9ba26e382dfc9f4a93f2a8d354a2d2020e187
Hitendra singh <hitendrasingh5489@gmail.com>
2025-09-10 15:58:20 +0530

    yd

--- Lec.c
+++ Lec.c
@@ -1,30 +1,69 @@
+// #include <stdio.h>
+// int main()
+// {
+//     for (int i = 0; i < 4; i++)
+//     {
+//         for (int j = 0; j <= i; j++)
+//         {
+//             printf("*");
+//         }
+//         for (int k = 3; k > i; k--)
+//         {
+//             printf(" ");
+//         }
+//         for (int p = 3; p > i; p--)
+//         {
+//             printf(" ");
+//         }
+//         for (int z = 0; z <= i; z++)
+//         {
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+
+//     for (int i = 0; i < 4; i++)
+//     {
+//         for (int j = 3; j > i; j--)
+//         {
+//             printf("*");
+//         }
+//         for (int k = 0; k <= i; k++)
+//         {
+//             printf(" ");
+//         }
+//         for (int k = 0; k <= i; k++)
+//         {
+//             printf(" ");
+//         }
+//         for (int j = 3; j > i; j--)
+//         {
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+// }
+/////////////////////////////////////////////////////////////////
 #include <stdio.h>
 
 int main()
 {
-    int n = 4; 
+    int n = 5;
 
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-   
-        for (int j = 0; j <= i; j++)
+        for (int j = 1; j <= n; j++)
         {
-            printf("*");
+            if (i == 1 || i == n || j == 1 || j == n)
+            {
+                printf("*"); 
+            }
+            else
+            {
+                printf(" "); 
+            }
         }
-
-      
-        for (int k = 0; k < 2 * (n - i - 1); k++)
-        {
-            printf(" ");
-        }
-
-        
-        for (int j = 0; j <= i; j++)
-        {
-            printf("*");
-        }
-
-        printf("\n");
+        printf("\n"); 
     }
 
     return 0;
