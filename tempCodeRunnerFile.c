d23dfae97274a080d67a18baa44b1b6def0516fd
Hitendra singh <hitendrasingh5489@gmail.com>
2025-09-10 15:10:12 +0530

    new

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,11 @@
+     for (int k = 4; k > i; k--)
+            {
+                printf(" ");
+            }
+        }
+        {
+            for (int p = 3; p >= i; p--)
+            {
+                printf("*");
+            }
+        }
