9474798e2bf2b94fb17595f35f00427f04c85354
Hitendra singh <hitendrasingh5489@gmail.com>
2025-09-15 14:03:31 +0530

    new

--- L12.c/hello.c
+++ L12.c/hello.c
@@ -1,9 +1,44 @@
 #include<stdio.h>
+// int main()
+// {
+//     int arr[5] = {1, 2, 4};
+//     int sum=0;
+//     for (int i=0;i<3;i++)
+//     {
+//         sum= sum+arr[i];
+//     }
+//     printf("%d", sum);
+// }
+
+
+///to print max value from array;
+// #include <stdio.h>
+// int main()
+// {
+//     int arr[5] = {1, 2, 4};
+//     int max=arr[0];
+//     for (int i=0;i<3;i++)
+//     {
+//        if(arr[i]>max){
+//         max=arr[i];
+//        };
+//     }
+//     printf("%d", max);
+// }
+
+
+///TO PRINT MIN VALUE FROM ARRAY;
+#include <stdio.h>
 int main()
 {
     int arr[5] = {1, 2, 4};
-    for (int i=0;i<3;i++)
+    int max = arr[0];
+    for (int i = 0; i < 3; i++)
     {
-        printf("%d", arr[i]);
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        };
     }
+    printf("%d", max);
 }
