98ef1f61d160124322c9cb6f6cc94cbbf529a4ae
Hitendra singh <hitendrasingh5489@gmail.com>
2025-09-23 14:02:08 +0530

    Add function to calculate the maximum sum of a subarray in hello.c

--- Lectures/L18/hello.c
+++ Lectures/L18/hello.c
@@ -86,3 +86,22 @@
 //         printf("Palindrome\n");
 //     }
 // }
+
+//////SUM OF ARRAY
+#include<limits.h>
+
+int main(){
+    int arr[5]={1,2,-4,3,5};
+ int maxSum=INT_MIN;
+
+ for(int i=0;i<5;i++){
+    int currSum=0;
+    for(int j=1;j<6;j++){
+        currSum=currSum+arr[j];
+        if(currSum>maxSum) maxSum=currSum;
+
+    }
+ }
+ printf("%d",maxSum);
+}
+
