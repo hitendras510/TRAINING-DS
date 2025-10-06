d3ea38da524aef1806222d9a0093534958be53a5
Hitendra singh <hitendrasingh5489@gmail.com>
2025-10-06 15:05:42 +0530

    Refactor hello.c: remove commented code and clean up formatting

--- Lectures/L20/hello.c
+++ Lectures/L20/hello.c
@@ -1,21 +1,49 @@
 #include<stdio.h>
-#include<string.h>
+#include <string.h>
 int main(){
 
+
     // char name[] = "hii"; 
-    // printf("Name: %s", name);
-    ///////////////////////////////////
-    
-char name[] = "wow"; 
-int ln= strlen(name);
-int isTrue=1;
-for(int i=0;i<ln/2;i++){
-if(name[i]!=name[ln-i-1]){
-        isTrue=0;
-        break;
-    }
-}
-printf("%s\n",name);
+// for(int i=0;name[i]!='\0';i++){
+//     printf("Name: %c \n", name[i]);
+// };
+
+
+// for(int i=0;name[i]!='\0';i++){
+//     printf("%s",name);
+// }
+
+/////////////////////////////////
+
+// char name[]="HELLO";
+// int ln= strlen(name);
+// for(int i<=3;name[i]!='\0';i--){
+//     printf("%s\n",name);
+// }
+
+//////////////
+
+// char name[]="hii";
+// char data[10];
+// for(int i=0;name[i]!='\0';i++){
+//     data[i] = name[i];
+// }
+// printf("%s",data);
+//////////////////////
+
+
+
+
+// char name[] = "wow"; 
+// int ln= strlen(name);
+// int isTrue=1;
+// for(int i=0;i<ln/2;i++){
+// if(name[i]!=name[ln-i-1]){
+//         isTrue=0;
+//         break;
+//     }
+// }
+// printf("%s\n",name);
 
 
 
