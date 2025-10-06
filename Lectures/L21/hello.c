617fa80b5773a8a93424addf54ff616363a48f60
Hitendra singh <hitendrasingh5489@gmail.com>
2025-10-06 13:12:33 +0530

    Refactor hello.c to remove commented code and enhance character printing logic

--- Lectures/L21/hello.c
+++ Lectures/L21/hello.c
@@ -1,8 +1,18 @@
 #include<stdio.h>
 int main(){
 
-char name[] = "hii"; 
-printf("Name: %c", name[0]);
+    // char name[] = "hii"; 
+    // printf("Name: %s", name);
+    ///////////////////////////////////
+    
+    char name[] = "hitendra"; 
+    int i = 0;
+
+    while(name[i] != '\0'){
+        printf("%c\n",name[i]);
+        i++;
+    }
+    
 
 
 
