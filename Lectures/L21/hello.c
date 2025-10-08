fb52a09c393e3723587503af4bcb29181c3a7769
Hitendra singh <hitendrasingh5489@gmail.com>
2025-10-08 13:30:01 +0530

    new

--- Lectures/L21/hello.c
+++ Lectures/L21/hello.c
@@ -1,20 +1,46 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-    char str[]="hello";
-    int ln= strlen(str);
-    int count=0;
-    for(int i=0;i<ln;i++){
-        count = 0;
-        for(int j=0;j<i;j++){
-            if(str[i]==str[j]){
-                count ++;
-                break;
-            }
-        }
-        if(count == 0)
-        {
-            printf("%c",str[i]);
-        }
+//     char str[]="hello";
+//     int ln= strlen(str);
+//     int count=0;
+//     for(int i=0;i<ln;i++){
+//         count = 0;
+//         for(int j=0;j<i;j++){
+//             if(str[i]==str[j]){
+//                 count ++;
+//                 break;
+//             }
+//         }
+//         if(count == 0)
+//         {
+//             printf("%c",str[i]);
+//         }
+//     }
+
+char str[]="aiouabvs";
+int vcount=0;
+int ccount=0;
+int ln= strlen(str);
+for(int i=0;i<ln;i++){
+    if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u'){
+        vcount++;
+    }else{
+        ccount++;
     }
+    printf("%d %d",vcount,ccount);
 }
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
