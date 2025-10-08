98e398a7a0acfcc69d9bac0df3dcc42ec9a99dfc
Hitendra singh <hitendrasingh5489@gmail.com>
2025-10-08 15:24:38 +0530

    Refactor hello.c: clean up character frequency counting logic and add a command marker

--- Lectures/L21/hello.c
+++ Lectures/L21/hello.c
@@ -53,24 +53,7 @@ int main(){
 //     }
 // }
 
-char str[]="abcd";
-int freq[256]={0};
-int ln = strlen(str);
-for(int i=0;i<ln;i++){
-    freq[str[i]]++;
-}for(int i=0;i<256;i++){
-    if(freq[i]>0){
-
-        printf("%c: %d\n",i,freq[i]);
-    }
-}
-
-
-
-
-
-
-
-}
-
-
+/*************  ✨ Windsurf Command 🌟  *************/
+char str[] = "abcd";
+int freq[256] = {0};
+i
