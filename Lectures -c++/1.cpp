8ed4ef80489805a3998c848ad4bbfb7307624d19
Hitendra singh <hitendrasingh5489@gmail.com>
2025-10-10 12:59:24 +0530

    Implement code changes to enhance functionality and improve performance

--- Lectures -c++/1.cpp
+++ Lectures -c++/1.cpp
@@ -1,12 +1,51 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
 
-void dis P()
-int main()
-{
+struct Node {
+    int data;
+    Node *next;
+};
 
+void addNode(Node*& first, int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->next = first;
+    first = temp;
+}
 
+void display(Node* first) {
+    Node* temp = first;
+    while (temp != nullptr) {
+        std::cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    std::cout << "nullptr" << std::endl;
+}
 
+int main() {
+    Node *first = nullptr;
+
+    int numNodes;
+    std::cout << "How many nodes do you want to add? ";
+    std::cin >> numNodes;
+
+    for (int i = 0; i < numNodes; ++i) {
+        int value;
+        std::cout << "Enter value for node " << i + 1 << ": ";
+        std::cin >> value;
+        addNode(first, value);
+    }
+
+    std::cout << "\nThe linked list is: ";
+    display(first);
+
+    std::cout << "\nDeleting all nodes..." << std::endl;
+    Node* temp = first;
+    while (temp != nullptr) {
+        Node* nextNode = temp->next;
+        delete temp;
+        temp = nextNode;
+    }
+    first = nullptr;
 
     return 0;
 }
