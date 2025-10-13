49daa24a07a276faee60639b4ad43ce2c1308787
Hitendra singh <hitendrasingh5489@gmail.com>
2025-10-13 10:49:24 +0530

    h

--- Lectures -c++/1.cpp
+++ Lectures -c++/1.cpp
@@ -5,53 +5,90 @@
 #define null 0
 using namespace std;
 
- struct node
+struct node
 {
-  int data;
+    int data;
     node *next;
 };
-node *first,*temp,*ttemp,*p,*q,*r,*ptemp,*pttemp,
-*ntemp,*nttemp;
+node *first, *temp, *ttemp, *p, *q, *r, *ptemp, *pttemp, *ntemp, *nttemp;
 
 void init()
 {
- first=temp=ttemp=null;
+    first = temp = ttemp = null;
 }
+
 void addnode(int val)
 {
-  temp=first;
-  while(temp->next!=null)
-  {
-  temp=temp->next;
-
-  }
-  ttemp=new node;
-  ttemp->data=val;
-  ttemp->next=null;
-  temp->next=ttemp;
+    temp = first;
+    while (temp->next != null)
+    {
+        temp = temp->next;
+    }
+    ttemp = new node;
+    ttemp->data = val;
+    ttemp->next = null;
+    temp->next = ttemp;
 }
+
 void createfirst(int val)
 {
- first=new node;
-  first->data=val;
-  first->next=null;
+    first = new node;
+    first->data = val;
+    first->next = null;
 }
+
 void disp()
 {
- temp=first;
- while(temp!=null)
- {
-   cout<<temp->data<<endl;
-   temp=temp->next;
- }
+    temp = first;
+    while (temp != null)
+    {
+        cout << temp->data << endl;
+        temp = temp->next;
+    }
 }
+
 void Add_After(int x, int y)
 {
     temp = first;
     while (temp->data != x)
         temp = temp->next;
+
     ttemp = temp->next;
-   p = new node;
+    p = new node;
+    p->data = y;
+    p->next = ttemp;
+    temp->next = p;
+}
+
+void Add_before(int x, int y)
+{
+    temp = first;
+
+
+    if (first->data == x)
+    {
+        p = new node;
+        p->data = y;
+        p->next = first;
+        first = p;
+        return;
+    }
+
+    
+    while (temp->next != null && temp->next->data != x)
+    {
+        temp = temp->next;
+    }
+
+    if (temp->next == null)
+    {
+        cout << "Node not found!" << endl;
+        return;
+    }
+
+    
+    ttemp = temp->next;
+    p = new node;
     p->data = y;
     p->next = ttemp;
     temp->next = p;
@@ -59,12 +96,16 @@ void Add_After(int x, int y)
 
 int main()
 {
-	init();
-	createfirst(10);
-	addnode(20);
-	addnode(30);
-	addnode(50);
-	addnode(60);
-	disp();
+    init();
+    createfirst(10);
+    addnode(20);
+    addnode(30);
+    addnode(50);
+    addnode(60);
+    addnode(70);
 
+    Add_before(50, 100);
+    Add_After(30, 200);
+
+    disp();
 }
