d94ffcb29ec02be3a71296dcd4afa43c32875c23
Hitendra singh <hitendrasingh5489@gmail.com>
2025-10-13 13:16:38 +0530

    w

---
+++ Lectures  - c/L22/1.c
@@ -0,0 +1,70 @@
+// // DMA = dynamic memory allocation;
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+
+    // int n;
+    // printf("Enter size:");
+    // scanf("%d",&n);
+
+    // int *arr = (int *)malloc(n*sizeof(int));
+
+    // printf("Enter  elements:\n");
+    // for(int i=0;i<n;i++){
+    //     scanf("%d",&arr[i]);
+    // }
+    // for(int i=0;i<n;i++){
+    //     printf("%d ",arr[i]);
+    // }
+
+
+  
+        // int n;
+        // printf("Enter size: ");
+        // scanf("%d", &n);
+
+        // int *arr = (int *)malloc(n * sizeof(int));
+
+        // printf("Enter %d elements: ", n);
+        // for (int i = 0; i < n; i++)
+        // {
+        //     scanf("%d", &arr[i]);
+        // }
+
+        // printf("\nElements in order are:\n");
+
+        // // 1.  zeros
+        // for (int i = 0; i < n; i++)
+        // {
+        //     if (arr[i] == 0)
+        //         printf("%d ", arr[i]);
+        // }
+
+        // // 2.  negative numbers
+        // for (int i = 0; i < n; i++)
+        // {
+        //     if (arr[i] < 0)
+        //         printf("%d ", arr[i]);
+        // }
+
+        // // 3.  positive numbers
+        // for (int i = 0; i < n; i++)
+        // {
+        //     if (arr[i] > 0)
+        //         printf("%d ", arr[i]);
+        // }
+
+        // free(arr);
+
+        ///////////////
+        
+
+
+
+
+    
+
+
+
+    }
