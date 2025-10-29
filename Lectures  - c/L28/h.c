38322ccecc059c5e83616fb9c7830ce9d9765f62
Hitendra singh <hitendrasingh5489@gmail.com>
2025-10-29 13:10:31 +0530

    new

--- Lectures  - c/L28/h.c
+++ Lectures  - c/L28/h.c
@@ -8,12 +8,14 @@ int main(){
     // }
     // printf("%d",xor);
 
-    int a = 5;
-    int b = 3;
-    a = a ^ b;
-    b = a ^ b;
-    a = a ^ b;
+    // int a = 5;
+    // int b = 3;
+    // a = a ^ b;
+    // b = a ^ b;
+    // a = a ^ b;
     
 
-    printf("a=%d , b=%d", a,b);
+    // printf("a=%d , b=%d", a,b);
+
+    
 }
