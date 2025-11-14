be6a990aafd2185085d7601cb15ca7672f09a20d
Hitendra singh <hitendrasingh5489@gmail.com>
2025-11-14 18:13:16 +0530

    new

--- Lectures -c++/Array/LinearSearch.cpp
+++ Lectures -c++/Array/LinearSearch.cpp
@@ -1,1 +1,20 @@
-//
+  #include<iostream>
+  using namespace std;
+  
+  int linearSearch(int arr[], int sz, int target){
+    for(int i=0;i<sz;i++){
+        if(arr[i]==target){
+            return i;
+        }
+    }
+    return -1;
+  }
+  int main()
+  {
+    int arr[]={10,20,30,40};
+    int sz=4;
+    int target=50;
+
+    cout<<linearSearch(arr,sz,target)<<endl;
+    return 0;
+  }
