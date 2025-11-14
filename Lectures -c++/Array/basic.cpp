de8cb31c3d245f30f46d6c3e563c8f210f4930b9
Hitendra singh <hitendrasingh5489@gmail.com>
2025-11-14 14:55:51 +0530

    new

--- Lectures -c++/Array/basic.cpp
+++ Lectures -c++/Array/basic.cpp
@@ -35,8 +35,36 @@ int main() {
 
 
 
+    int size = 5;
+    int arr[size];
+    
+    int largest = INT_MIN;
+    int smallest = INT_MAX;
 
+    int indexLargest = 0;
+    int indexSmallest = 0;
 
+    cout << "Enter 5 numbers: " << endl;
+
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+
+        if (arr[i] > largest) {
+            largest = arr[i];
+            indexLargest = i;   // store index of largest
+        }
+
+        if (arr[i] < smallest) {
+            smallest = arr[i];
+            indexSmallest = i;  // store index of smallest
+        }
+    }
+
+    cout << "Largest number: " << largest << endl;
+    cout << "Smallest number: " << smallest << endl;
+
+    cout << "Index of largest: " << indexLargest << endl;
+    cout << "Index of smallest: " << indexSmallest << endl;
 
     return 0;
 }
