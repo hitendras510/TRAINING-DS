4e69c900e726d21de96eb601dd31841fd38cd429
Hitendra singh <hitendrasingh5489@gmail.com>
2025-11-14 14:48:07 +0530

    new

---
+++ Lectures -c++/Array/basic.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<climits>   // for INT_MAX
+using namespace std;
+
+int main() {
+    // int size = 5;
+    // int nums[size];
+    // int smallest = INT_MAX;
+
+    // cout << "Enter 5 numbers: " << endl;
+
+    // for (int i = 0; i < size; i++) {
+    //     cin >> nums[i];
+
+    //     if (nums[i] < smallest) {
+    //         smallest = nums[i];
+    //     }
+    // }
+
+    // cout << "The smallest number is: " << smallest << endl;
+
+
+
+// int size=5;
+// int arr[size];
+// int largest=INT_MIN;
+// cout<<"Enter 5 numbers: "<<endl;
+// for(int i=0;i<size;i++){
+//     cin>>arr[i];
+//     if(arr[i]>largest){
+//         largest=arr[i];
+//     }
+// }
+// cout<<"The largest number is: "<<largest<<endl;
+
+
+
+
+
+
+    return 0;
+}
