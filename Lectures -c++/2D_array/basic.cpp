c03a95d5b7fd14c7fa03ab513cbefce3b7608464
Hitendra singh <hitendrasingh5489@gmail.com>
2025-11-15 17:21:26 +0530

    new

---
+++ Lectures -c++/2D_array/basic.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int matrix[4][3]={{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+int rows=4;
+int cols=3;
+
+cout<< matrix[0][0]<<endl;
+cout<< matrix[0][1]<<endl;
+cout<< matrix[0][2]<<endl;
+cout<< matrix[1][0]<<endl;
+cout<< matrix[1][1]<<endl;
+cout<< matrix[1][2]<<endl;
+cout<< matrix[2][0]<<endl;
+cout<< matrix[2][1]<<endl;
+cout<< matrix[2][2]<<endl;
+cout<< matrix[3][0]<<endl;
+cout<< matrix[3][1]<<endl;
+cout<< matrix[3][2]<<endl;
+
+}
