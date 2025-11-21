37b3b0b4feb4247fe573bc1f8f8c1a6721c92f83
Hitendra singh <hitendrasingh5489@gmail.com>
2025-11-21 22:28:39 +0530

    new

---
+++ Lectures - c++/Recursion/Subsets.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void getAllSubsets(vector<int>& nums, vector<int>& ans, int i, vector<vector<int>>& allSubsets) {
+    if (i == nums.size()) {
+        allSubsets.push_back(ans);
+        return;
+    }
+
+    // include nums[i]
+    ans.push_back(nums[i]);
+    getAllSubsets(nums, ans, i + 1, allSubsets);
+
+    // exclude nums[i]
+    ans.pop_back();
+    getAllSubsets(nums, ans, i + 1, allSubsets);
+}
+
+vector<vector<int>> subsets(vector<int>& nums) {
+    vector<vector<int>> allSubsets;
+    vector<int> ans;
+    getAllSubsets(nums, ans, 0, allSubsets);
+    return allSubsets;
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3};
+    vector<vector<int>> result = subsets(nums);
+
+    for (auto subset : result) {
+        cout << "[ ";
+        for (int x : subset) cout << x << " ";
+        cout << "]" << endl;
+    }
+
+    return 0;
+}
