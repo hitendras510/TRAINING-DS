70fb324880a5146dde258ed36ed13e185d8d79be
Hitendra singh <hitendrasingh5489@gmail.com>
2025-11-21 14:33:41 +0530

    new

--- Lectures - c++/Recursion/PrintSubsets.cpp
+++ Lectures - c++/Recursion/PrintSubsets.cpp
@@ -1,1 +1,30 @@
 //using backtracking.
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void printSubset(vector<int> &arr, vector<int> &ans, int i){
+    if(i == arr.size()){
+        for(int val : ans){
+            cout << val << " ";
+        }
+        cout<<endl;
+        return;
+    }
+
+    //include the element
+    ans.push_back(arr[i]);
+    printSubset(arr, ans, i+1);
+
+    //exclude the element
+    ans.pop_back();
+    printSubset(arr, ans, i+1);
+}
+
+int main(){
+    vector<int> arr = {1,2,3};
+    vector<int> ans; //store subsets
+
+    printSubset(arr, ans, 0);
+    return 0;
+}
