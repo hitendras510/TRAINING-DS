9e1d22360559b17f43cb8e6def475cbe129edf9a
Hitendra singh <hitendrasingh5489@gmail.com>
2025-11-22 20:49:57 +0530

    new

--- Lectures - c++/Binary Tree/basic.cpp
+++ Lectures - c++/Binary Tree/basic.cpp
@@ -1,1 +1,27 @@
 //Build a tree.
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+static int idc = -1;
+Node* buildTree(vector<int> preorder){
+    idx++;
+
+    if(preorder[idx]== -1){
+        return NULL;
+    }
+
+}
