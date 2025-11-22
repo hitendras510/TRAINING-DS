2b6b36e08e30a2efe4e5b72d285e98edd2ab826c
Hitendra singh <hitendrasingh5489@gmail.com>
2025-11-22 22:49:36 +0530

    new

--- Lectures - c++/Binary Tree/basic.cpp
+++ Lectures - c++/Binary Tree/basic.cpp
@@ -16,7 +16,7 @@ class Node{
     }
 };
 
-static int idc = -1;
+static int idx = -1;
 Node* buildTree(vector<int> preorder){
     idx++;
 
@@ -24,4 +24,21 @@ Node* buildTree(vector<int> preorder){
         return NULL;
     }
 
+    Node* root = new Node(preorder[idx]);
+    root->left = buildTree(preorder);
+    root->right = buildTree(preorder);
+
+    return root;
+
+}
+
+int main(){
+    vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+
+    Node* root = buildTree(preorder);
+    cout<<root->data<<endl;
+    cout<<root->left->data<<endl;
+    cout<<root->right->data<<endl;
+
+    return 0;
 }
