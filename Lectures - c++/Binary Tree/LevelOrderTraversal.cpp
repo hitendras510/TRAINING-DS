1fc8a27b64f1ba0358b4ba23bd1d569ffb5badd1
Hitendra singh <hitendrasingh5489@gmail.com>
2025-11-23 00:10:28 +0530

    new

--- Lectures - c++/Binary Tree/LevelOrderTraversal.cpp
+++ Lectures - c++/Binary Tree/LevelOrderTraversal.cpp
@@ -5,51 +5,61 @@
 #include <queue>
 using namespace std;
 
-class Node {
-public:
+class Node{
+    public; 
     int data;
     Node* left;
     Node* right;
-
     Node(int val) {
         data = val;
         left = NULL;
         right = NULL;
     }
-};
+}
 
-// Function to build tree from In Order vector with -1 as NULL
-Node* buildTree(vector<int>& arr, int& i) {
-    if (i >= arr.size() || arr[i] == -1) /**conditon for NULL*/ {
+Node* buildTree( vector<int>& arr, int& i) {
+    if( i >= arr.size() || arr[i] == -1){
         i++;
         return NULL;
     }
-
     Node* root = new Node(arr[i]);
     i++;
     root->left = buildTree(arr, i);
     root->right = buildTree(arr, i);
-
     return root;
 }
 
-// inOrder traversal
-void inOrderTraversal(Node* root){
-    if(root == NULL) return;
-    inOrderTraversal(root->left);
-    cout<<root->data<<" ";
-    inOrderTraversal(root->right);
+void levelOrder(Node* root){
+    queue<Node*> q;
+    q.push(root);
+
+    while(q.size()>0){
+        Node* curr = q.front();
+        q.pop();
+
+        cout<<curr->data<<" ";
+
+        if(curr->left!= NULL){
+            q.push(curr->left);
+        }
+        if(curr->right!= NULL){
+            q.push(curr->right);
+        }
+    }
+    cout<<endl;
 }
 
-int main() {
-    vector<int> inOrderData = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+
+
+int main(){
+vector<int> levelOrderData = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
     int index = 0;
-
-    Node* root = buildTree(inOrderData, index);
-
-    cout << "InOrder Traversal: ";
-    inOrderTraversal(root);
-    cout << endl;
-
+    
+    Node* root = buildTree(levelOrderData, index);
+    
+    cout<<"Level Order Traversal: ";
+    levelOrder(root);
     return 0;
+
+
 }
